fe90a3afb1915fd929ba989778061dc2691e350e
Juhyun <s2019w09@e-mirim.hs.kr>
2020-11-08 04:58:22 +0900

    add 'listActivity'

--- app/src/main/java/kr/hs/emirim/s2019w27/calender/DB/AppDatabase.java
+++ app/src/main/java/kr/hs/emirim/s2019w27/calender/DB/AppDatabase.java
@@ -4,9 +4,9 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 import kr.hs.emirim.s2019w27.calender.AddActivity;
-import kr.hs.emirim.s2019w27.calender.MainActivity;
+import kr.hs.emirim.s2019w27.calender.GVCalendarActivity;
 
-@Database(entities = {Memo.class}, version = 4, exportSchema = true)
+@Database(entities = {Memo.class}, version = 4, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract MemoDAO memoDAO();
 
