8eeba40e7ef5951ce6dbb6271321d7139038b3c8
맹수연 <sooyeon030908@gmail.com>
2020-11-08 03:34:21 +0900

    modify files

--- app/src/main/java/kr/hs/emirim/s2019w27/calender/DB/AppDatabase.java
+++ app/src/main/java/kr/hs/emirim/s2019w27/calender/DB/AppDatabase.java
@@ -3,11 +3,7 @@ package kr.hs.emirim.s2019w27.calender.DB;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import kr.hs.emirim.s2019w27.calender.AddActivity;
-import kr.hs.emirim.s2019w27.calender.MainActivity;
-
 @Database(entities = {Memo.class}, version = 4, exportSchema = true)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract MemoDAO memoDAO();
-
 }
