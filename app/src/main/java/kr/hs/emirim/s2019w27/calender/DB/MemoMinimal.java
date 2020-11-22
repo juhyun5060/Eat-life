cbb41b82df1d1386e059063c63a7ba70d969702e
Juhyun <s2019w09@e-mirim.hs.kr>
2020-11-23 06:58:26 +0900

    edit ListActivity

--- app/src/main/java/kr/hs/emirim/s2019w27/calender/DB/MemoMinimal.java
+++ app/src/main/java/kr/hs/emirim/s2019w27/calender/DB/MemoMinimal.java
@@ -1,12 +1,21 @@
 package kr.hs.emirim.s2019w27.calender.DB;
 
+import androidx.room.ColumnInfo;
+
 public class MemoMinimal {
+    @ColumnInfo(name = "title")
     String title;
+
+    @ColumnInfo(name = "date")
     String date;
 
-    public MemoMinimal(String title, String date) {
+    @ColumnInfo(name = "imgUri")
+    String imgUri;
+
+    public MemoMinimal(String title, String date, String imgUri) {
         this.title = title;
         this.date = date;
+        this.imgUri = imgUri;
     }
 
     public String getTitle() {
@@ -23,5 +32,13 @@ public class MemoMinimal {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
     }
 }
--- app/src/main/java/kr/hs/emirim/s2019w27/calender/listView/RecyclerAdapter.java
+++ app/src/main/java/kr/hs/emirim/s2019w27/calender/listView/RecyclerAdapter.java
@@ -1,22 +1,28 @@
 package kr.hs.emirim.s2019w27.calender.listView;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
+import kr.hs.emirim.s2019w27.calender.AddActivity;
 import kr.hs.emirim.s2019w27.calender.DB.Memo;
 import kr.hs.emirim.s2019w27.calender.DB.MemoMinimal;
 import kr.hs.emirim.s2019w27.calender.R;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
-
 
     private ArrayList<MemoMinimal> memoData = new ArrayList<>();
 
@@ -29,17 +35,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.onBind(memoData.get(position), position);
+        holder.onBind(memoData.get(position));
     }
 
     @Override
     public int getItemCount() {
         return memoData.size();
-    }
-
-    public void addItem(MemoMinimal memo) {
-        memoData.add(memo);
-        notifyDataSetChanged();
     }
 
     public void addItems(MemoMinimal memo) {
@@ -50,7 +51,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     public class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView title;
         private TextView date;
-//        private ImageView image;
+        private ImageView image;
 
 
         public MyViewHolder(@NonNull View itemView) {
@@ -58,13 +59,13 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
             title = itemView.findViewById(R.id.listTitle);
             date = itemView.findViewById(R.id.listDate);
-//            image = itemView.findViewById(R.id.listImg);
+            image = itemView.findViewById(R.id.listImg);
         }
 
-        public void onBind(MemoMinimal memo, int position) {
+        public void onBind(MemoMinimal memo) {
             title.setText(memo.getTitle());
             date.setText(memo.getDate());
-//            image.setImageResource(R.drawable.icon);
+            image.setImageURI(Uri.parse(memo.getImgUri()));
         }
     }
 }
